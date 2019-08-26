223d6505512870e64f9b6f784cb9bc72b6229140
longwei <longwei_cs@sjtu.edu.cn>
2019-08-26 18:45:58 +0800

    add barplot for selfv.eps

--- result/barplot.py
+++ result/barplot.py
@@ -118,7 +118,43 @@ def plot_agg_slf():
     plt.savefig("agg_slf.eps")
 
 
+def plot_selfv():
+    df = pd.read_csv('res18fv.csv').set_index('fv')
+    fvs = np.array(df.index)
+    metrics = np.array(df.columns)
+
+    fig, ax = plt.subplots(figsize=(20, 5))
+    bar_width = 0.16
+    opacity = 0.8
+    plt.style.use('ggplot')
+
+    pos = np.arange(len(metrics))
+    for i, m in enumerate(fvs):
+        plt.bar(pos + i*bar_width, df.loc[m], bar_width,
+                alpha=opacity, label=m)
+
+    metrics_labels = [
+        'subset_accuracy', 'example_accuracy', 'example_precision',
+        'example_recall', 'example_f1',
+        'label_accuracy', 'label_precision', 'label_recall', 'label_f1']
+    plt.xticks(pos+0.4, metrics_labels)
+    ax.tick_params(axis='x', length=0, pad=8)
+
+    ax.set_xlim(left=-0.2, right=9.0)
+    ax.set_ylim(bottom=0.2, top=0.95)
+    leg = plt.legend(loc='best', ncol=5, mode='expand',
+                     shadow=True, fancybox=True)
+    leg.get_frame().set_alpha(0.4)
+    plt.tight_layout()
+    # plt.legend(title=r'Different features',
+    #            loc='upper left', bbox_to_anchor=(1, 1))
+    # plt.tight_layout(rect=[0, 0, 0.85, 1.0])
+    # plt.show()
+    plt.savefig('selfv.eps')
+
+
 if __name__ == "__main__":
     # plot_loss()
     # plot_agg_res18()
-    plot_agg_slf()
+    # plot_agg_slf()
+    plot_selfv()
